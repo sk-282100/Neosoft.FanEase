948f6e08040dc30836e07614aa9b42e7a5d0a62d
kajalpadhiyar <kajalpadhiyar962@gmail.com>
2021-10-25 19:16:15 +0530

    Updated Forgot Password

--- Neosoft.FAMS/Neosoft.FAMS.WebApp/Controllers/LoginController.cs
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/Controllers/LoginController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Neosoft.FAMS.WebApp.Models.LoginModel.Login;
 using Neosoft.FAMS.WebApp.Models.LoginModel.ResetPassword;
+using System.Net.Mail;
+using System.Security.Cryptography;
 
 namespace Neosoft.FAMS.WebApp.Controllers
 {
@@ -67,9 +69,44 @@ namespace Neosoft.FAMS.WebApp.Controllers
         [HttpPost]
         public ActionResult SendOtp(Login login)
         {
+            Random rdm = new Random();
+            int code = rdm.Next(1000, 9999);
+            string to = login.Username;
+            string subject = "For reset password";
+            string body = $"Your code is {code}";
+            MailMessage mm = new MailMessage();
+            mm.To.Add(to);
+            mm.Subject = subject;
+            mm.Body = body;
+            mm.From = new MailAddress("kajalpadhiyar962@gmail.com");
+            mm.IsBodyHtml = false;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("kajalpadhiyar962@gmail.com", "Nisha@11");
+            smtp.Send(mm);
+            DateTime currentTime = DateTime.Now;
+            DateTime x30MinsLater = currentTime.AddMinutes(30);
+            Console.WriteLine(string.Format("{0} {1}", currentTime, x30MinsLater));
+            return RedirectToAction("CheckOtp", "Login");
+        }
+       
+
+        public ActionResult CheckOtp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CheckOtp(string Otp)
+        {
+            string abc=Otp;
+            DateTime currentTime = DateTime.Now;
+            DateTime x30MinsLater = currentTime.AddMinutes(30);
+            int result = DateTime.Compare(x30MinsLater,currentTime);
             return RedirectToAction("ForgotPassword", "Login");
         }
-        public IActionResult Logout()
+            public IActionResult Logout()
         {
             HttpContext.Session.Remove("Username");
             return RedirectToAction("Home", "Login");
