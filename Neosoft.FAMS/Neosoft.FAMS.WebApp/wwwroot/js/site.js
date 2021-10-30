47f9feada3119fe81ea1cd76d21663827dd7f1f8
aman1862000 <rjamans18@gmail.com>
2021-10-30 15:20:45 +0530

    ui modified-Aman

--- Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/site.js
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+console.log(1);
+
+$(document).ready(function () {
+    $('#myTable').DataTable();
+});
+   
