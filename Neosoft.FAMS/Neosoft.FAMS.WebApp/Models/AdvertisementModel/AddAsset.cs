eb5ebf114a3000151a761ec950db5928a86c3c53
sanahaju <sanahaju123@gmail.com>
2021-10-31 15:27:19 +0530

    AddAsset Validation-Sana

--- Neosoft.FAMS/Neosoft.FAMS.WebApp/Models/AdvertisementModel/AddAsset.cs
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/Models/AdvertisementModel/AddAsset.cs
@@ -15,17 +15,31 @@ namespace Neosoft.FAMS.WebApp.Models.AdvertisementModel
         [Required(ErrorMessage = "Title is required")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string Title { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+
+        [Required(ErrorMessage = "Start Time is required")]
+        [DisplayName("Start Time")]
+        public DateTime? StartTime { get; set; }
+
+        [Required(ErrorMessage = "End Time is required")]
+        [DisplayName("End Time")]
+        public DateTime? EndTime { get; set; }
+
+        [Required(ErrorMessage = "Content Type is required")]
+        [DisplayName("Content Type")]
         public short? ContentTypeId { get; set; }
 
         [DisplayName("Description")]
         [Required(ErrorMessage = "Description is required")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string Description { get; set; }
-        public string ImagePath { get; set; }
-        public string VideoPath { get; set; }
+
+        
+        [Required(ErrorMessage = "Url is required")]
+        [DisplayName("Url")]
         public string Url { get; set; }
+
+        [Required(ErrorMessage = "Placement is required")]
+        [DisplayName("Placement")]
         public long? PlacementId { get; set; }
         public long? CreatedBy { get; set; }
         public DateTime? CreatedOn { get; set; }
