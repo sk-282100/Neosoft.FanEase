3e6a60299036638bf53fc4b6a93d4fe3ec6446fc
rajb45 <rajmbhosale3@gmail.com>
2021-10-31 16:05:46 +0530

    Model Validations -Raj

--- Neosoft.FAMS/Neosoft.FAMS.WebApp/Models/VideoModel/AddVideo.cs
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/Models/VideoModel/AddVideo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace Neosoft.FAMS.WebApp.Models.VideoModel
 {
     public class AddVideo
     {
+        [Required(ErrorMessage = "Video Image is required")]
         public string VideoImage { get; set; }
+        [Required(ErrorMessage = "Date And Time is required")]
         public DateTime? appt { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(30,ErrorMessage="Title Length Cannot Exceed 30 characters")]
         public string Title { get; set; }
+        
         public short? VideoTypeId { get; set; }
+        [Required(ErrorMessage = "Video Url is required")]
         public string VideoUrl { get; set; }
         public string UploadVideoPath { get; set; }
         public short? VideoCategoryId { get; set; }
