8e53966fbfa4e843cf2f6a1df0dc08445a804b63
shankar <shankarkadam54@gmail.com>
2021-11-08 11:04:06 +0530

    Migration - shankar

---
+++ Neosoft.FAMS/src/Infrastructure/Neosoft.FAMS.Persistence/Migrations/20211103105049_shankar.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Neosoft.FAMS.Persistence.Migrations
+{
+    public partial class shankar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Decription",
+                table: "VideoDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("1babd057-e980-4cb3-9cd2-7fdd9e525668"),
+                column: "Date",
+                value: new DateTime(2022, 9, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(827));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("3448d5a4-0f72-4dd7-bf15-c14a46b26c00"),
+                column: "Date",
+                value: new DateTime(2022, 8, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(627));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("62787623-4c52-43fe-b0c9-b7044fb5929b"),
+                column: "Date",
+                value: new DateTime(2022, 3, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(789));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("adc42c09-08c1-4d2c-9f96-2d15bb1af299"),
+                column: "Date",
+                value: new DateTime(2022, 7, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(870));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("b419a7ca-3321-4f38-be8e-4d7b6a529319"),
+                column: "Date",
+                value: new DateTime(2022, 3, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(745));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                column: "Date",
+                value: new DateTime(2022, 5, 3, 16, 20, 48, 143, DateTimeKind.Local).AddTicks(322));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("3dcb3ea0-80b1-4781-b5c0-4d85c41e55a6"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(6392));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("771cca4b-066c-4ac7-b3df-4d12837fe7e0"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(6356));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("7e94bc5b-71a5-4c8c-bc3b-71bb7976237e"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(4556));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("86d3a045-b42d-4854-8150-d6a374948b6e"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(6266));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ba0eb0ef-b69b-46fd-b8e2-41b4178ae7cb"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(6493));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("e6a2679c-79a3-4ef1-a478-6f4c91b405b6"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(6424));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 3, 16, 20, 48, 145, DateTimeKind.Local).AddTicks(6461));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                columns: new[] { "CreatedDate", "DateOfJoining" },
+                values: new object[] { new DateTime(2021, 11, 3, 16, 20, 48, 144, DateTimeKind.Local).AddTicks(9642), new DateTime(2022, 5, 3, 16, 20, 48, 144, DateTimeKind.Local).AddTicks(7847) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Decription",
+                table: "VideoDetails");
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("1babd057-e980-4cb3-9cd2-7fdd9e525668"),
+                column: "Date",
+                value: new DateTime(2022, 9, 2, 11, 49, 34, 730, DateTimeKind.Local).AddTicks(146));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("3448d5a4-0f72-4dd7-bf15-c14a46b26c00"),
+                column: "Date",
+                value: new DateTime(2022, 8, 2, 11, 49, 34, 729, DateTimeKind.Local).AddTicks(9233));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("62787623-4c52-43fe-b0c9-b7044fb5929b"),
+                column: "Date",
+                value: new DateTime(2022, 3, 2, 11, 49, 34, 729, DateTimeKind.Local).AddTicks(9758));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("adc42c09-08c1-4d2c-9f96-2d15bb1af299"),
+                column: "Date",
+                value: new DateTime(2022, 7, 2, 11, 49, 34, 730, DateTimeKind.Local).AddTicks(275));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("b419a7ca-3321-4f38-be8e-4d7b6a529319"),
+                column: "Date",
+                value: new DateTime(2022, 3, 2, 11, 49, 34, 729, DateTimeKind.Local).AddTicks(9640));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                column: "Date",
+                value: new DateTime(2022, 5, 2, 11, 49, 34, 719, DateTimeKind.Local).AddTicks(6433));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("3dcb3ea0-80b1-4781-b5c0-4d85c41e55a6"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 2, 11, 49, 34, 731, DateTimeKind.Local).AddTicks(7001));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("771cca4b-066c-4ac7-b3df-4d12837fe7e0"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 2, 11, 49, 34, 731, DateTimeKind.Local).AddTicks(6795));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("7e94bc5b-71a5-4c8c-bc3b-71bb7976237e"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 2, 11, 49, 34, 730, DateTimeKind.Local).AddTicks(9207));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("86d3a045-b42d-4854-8150-d6a374948b6e"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 2, 11, 49, 34, 731, DateTimeKind.Local).AddTicks(6370));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ba0eb0ef-b69b-46fd-b8e2-41b4178ae7cb"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 2, 11, 49, 34, 731, DateTimeKind.Local).AddTicks(7569));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("e6a2679c-79a3-4ef1-a478-6f4c91b405b6"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 2, 11, 49, 34, 731, DateTimeKind.Local).AddTicks(7187));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923"),
+                column: "OrderPlaced",
+                value: new DateTime(2021, 11, 2, 11, 49, 34, 731, DateTimeKind.Local).AddTicks(7390));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                columns: new[] { "CreatedDate", "DateOfJoining" },
+                values: new object[] { new DateTime(2021, 11, 2, 11, 49, 34, 729, DateTimeKind.Local).AddTicks(5174), new DateTime(2022, 5, 2, 11, 49, 34, 728, DateTimeKind.Local).AddTicks(8363) });
+        }
+    }
+}
