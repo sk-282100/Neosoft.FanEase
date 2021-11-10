42564758165b98ee58168dccc9a0506b75f1191b
rajb45 <rajmbhosale3@gmail.com>
2021-11-10 16:43:52 +0530

    Thumbnail File Size of Video

--- Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/AddVideo.js
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/AddVideo.js
@@ -1,13 +1,25 @@
 ﻿function readURL(input) {
     if (input.files && input.files[0]) {
-        var reader = new FileReader();
+        var fileSize = $(input)[0].files[0].size;
+        if (fileSize <= 1000000) {
+            $('#errorFileSize').text("");
+            var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#thumbnail').attr('src', e.target.result)
-                .width(150)
-                .height(100);
-        };
-        reader.readAsDataURL(input.files[0]);
+            reader.onload = function (e) {
+                $('#thumbnail').attr('src', e.target.result)
+                    .width(150)
+                    .height(100);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+        else {
+            var $el = $('#videoThumbnail');
+            $el.wrap('<form>').closest('form').get(0).reset();
+            $el.unwrap();
+            $('#thumbnail').attr('src', null);
+            $('#errorFileSize').text("Max File is 1 MB");
+            
+        }
     }
 }
 
