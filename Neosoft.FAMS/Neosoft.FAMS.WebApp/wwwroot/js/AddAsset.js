80396c1c9cc117083c80224c5ab27e3dff2641c8
kajalpadhiyar <kajalpadhiyar962@gmail.com>
2021-11-10 17:13:01 +0530

    Client side file validation

---
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/AddAsset.js
@@ -0,0 +1,17 @@
+﻿function readURL(input) {
+    if (input.files && input.files[0]) {
+        var fileSize = $(input)[0].files[0].size;
+        if (fileSize <= 1000000) {
+            $('#errorFileSize').text("");
+            var reader = new FileReader();
+            reader.readAsDataURL(input.files[0]);
+        }
+        else {
+            var $el = $('#formFileMultiple');
+            $el.wrap('<form>').closest('form').get(0).reset();
+            $el.unwrap();
+            $('#errorFileSize').text("Max File is 1 MB");
+
+        }
+    }
+}
