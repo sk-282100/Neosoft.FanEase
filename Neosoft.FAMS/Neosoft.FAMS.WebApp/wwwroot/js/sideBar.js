3a42aebaab4ca5b981a8531a31a13ab14916acbf
aman1862000 <rjamans18@gmail.com>
2021-11-11 19:23:09 +0530

    redesign of navbar

---
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/sideBar.js
@@ -0,0 +1,13 @@
+﻿var setDefaultActive = function () {
+    var path = window.location.pathname;
+    console.log("hello");
+    var element = $(".nav-item a[href='" + path + "']");
+
+    element.addClass("myactive");
+}
+
+
+
+$(document).ready(function() {
+    setDefaultActive();
+});
