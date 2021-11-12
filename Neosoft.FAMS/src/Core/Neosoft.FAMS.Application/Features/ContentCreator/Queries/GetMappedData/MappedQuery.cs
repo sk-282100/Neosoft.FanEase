f620aa4dc211b6b2d0fe2239a8af4381cda101e2
kajalpadhiyar <kajalpadhiyar962@gmail.com>
2021-11-12 12:22:47 +0530

    Mapped API

---
+++ Neosoft.FAMS/src/Core/Neosoft.FAMS.Application/Features/ContentCreator/Queries/GetMappedData/MappedQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using Neosoft.FAMS.Application.Features.ContentCreator.Queries.GetAll;
+
+namespace Neosoft.FAMS.Application.Features.ContentCreator.Queries.GetMappedData
+{
+    public class MappedQuery : IRequest<MappedDto>
+    {
+        public long VideoId { get; set; }
+    }
+}
