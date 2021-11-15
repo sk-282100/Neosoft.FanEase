e3c2939929c516797d72e87fc5cadbc9115caf8d
sanahaju <sanahaju123@gmail.com>
2021-11-15 13:56:39 +0530

    Updated stepbar

--- Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/StepBar.js
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/wwwroot/js/StepBar.js
@@ -1,16 +1,25 @@
 ﻿var setDefaultActive = function () {
     var path = window.location.pathname;
-    
-    if (path.includes("Campaign")) {
-        path = "/Creator/AddCampaignView";
-        document.getElementById('bar1').style.backgroundColor = "#56baed";
+    if (path.includes("Video")) {
+        path = "/Creator/VideoTable";
+        var element = $(".nav-item a[href='" + path + "']");
+        element.addClass("myactive");
     }
-    else if (path.includes("Asset")) {
+    else if (path.includes("Campaign")) {
+        path = "/Creator/AddCampaignView";
+        var element = $(".nav-item a[href='" + path + "']");
+        element.addClass("myactive");
+        document.getElementById('bar1').style.backgroundColor = "#56baed";
+
+    } else if (path.includes("Asset")) {
         path = "/Creator/AddAsset";
+        var element = $(".nav-item a[href='" + path + "']");
+        element.addClass("myactive");
         document.getElementById('bar1').style.backgroundColor = "#56baed";
         document.getElementById('bar2').style.backgroundColor = "#56baed";
+
     }
-    
+
 }
 
 
@@ -18,34 +27,3 @@ $(document).ready(function () {
     setDefaultActive();
 });
 
-
-
-
-
-
-
-
-
-
-
-
-//function myFunction(id) {
-//    if (id == "bar1") {
-//        document.getElementById(id).style.backgroundColor = "#56baed";
-//    }
-//    else {
-//        document.getElementById(id).style.backgroundColor = "white";
-//    }
-
-//}
-
-//function myFunction1(id1, id2) {
-//    if (id1 == "bar1" && id2 == "bar2") {
-//        document.getElementById(id1).style.backgroundColor = "#56baed";
-//        document.getElementById(id2).style.backgroundColor = "#56baed";
-//    }
-//    else {
-//        document.getElementById(id1).style.backgroundColor = "white";
-//    }
-
-//}
