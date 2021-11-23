aeb23a7b3a48d6c1e471e6480e686814ead788bf
kajalpadhiyar <kajalpadhiyar962@gmail.com>
2021-11-23 15:05:39 +0530

    Is Liked By viewerId API

---
+++ Neosoft.FAMS/src/Core/Neosoft.FAMS.Application/Features/VideoPage/Query/CheckIsLikedById/CheckIsLikedByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace Neosoft.FAMS.Application.Features.VideoPage.Query.CheckIsLikedById
+{
+    public class CheckIsLikedByIdQuery : IRequest<bool>
+    {
+        public long VideoId { get; set; }
+        public long ViewerId { get; set; }
+    }
+}
