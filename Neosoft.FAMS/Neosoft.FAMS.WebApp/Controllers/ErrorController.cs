d1130fa2e752ef49b9def5ccb876ff774dc772ae
aman1862000 <rjamans18@gmail.com>
2021-11-26 15:00:24 +0530

    removed bugs

---
+++ Neosoft.FAMS/Neosoft.FAMS.WebApp/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace Neosoft.FAMS.WebApp.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+       //[Route("{id}")]
+        public IActionResult Error500()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            var statusCode = exception.Error.GetType().Name switch
+            {
+                "ArgumentException" => HttpStatusCode.BadRequest,
+                _ => HttpStatusCode.ServiceUnavailable
+            };
+            
+            
+            return View("Error500");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var statusCode = exception.Error.GetType().Name switch
+            {
+                "ArgumentException" => HttpStatusCode.BadRequest,
+                _ => HttpStatusCode.ServiceUnavailable
+            };
+            return Problem(detail: exception.Error.Message, statusCode: (int)statusCode);
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
