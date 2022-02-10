ce3ff7ffa3cbf70a5b7ce0a3b7fb47b7d82fa1f6
kajalpadhiyar <kajalpadhiyar962@gmail.com>
2022-02-10 15:03:32 +0530

    Bootstrap Added

--- FanEaseAngular/FanEaseAngular/src/app/app.module.ts
+++ FanEaseAngular/FanEaseAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewerComponent } from './viewer/viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule,
